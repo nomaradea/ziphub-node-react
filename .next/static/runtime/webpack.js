63b6b0087d93f5c08a351d9813a9d782c764a7b3
nomaradea <nomar.adea@gmail.com>
2020-03-19 12:20:12 +0800

    update gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "575296ddda968f7e3c6c";
+/******/ 	var hotCurrentHash = "2159b952060f7eccf0b1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
