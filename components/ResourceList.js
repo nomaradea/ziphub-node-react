1c77e78c90eb5e08bf8d9de3aef1288ce66bf77a
nomaradea <nomar.adea@gmail.com>
2020-03-19 09:50:03 +0800

    add ResourceListWithProducts class to ResourceList

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -1,3 +1,7 @@
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import { Card } from '@shopify/polaris'
+
 const GET_PRODUCTS_BY_ID = gql`
     query getProducts($id: [ID!]!) {
         nodes(ids: $ids) {
@@ -26,3 +30,24 @@ const GET_PRODUCTS_BY_ID = gql`
         }
     }
 `
+
+class ResourceListWithProducts extends React.Component {
+    render() {
+        return (
+            <Query query={GET_PRODUCTS_BY_ID}>
+                {({ data, loading, error }) => {
+                    if (loading) return <div>Loading...</div>
+                    if (error) return <div>{error.message}</div>
+                    console.log(data)
+                    return (
+                        <Card>
+                            <p>Hello from Ziphub</p>
+                        </Card>
+                    )
+                }}
+            </Query>
+        )
+    }
+}
+
+export default ResourceListWithProducts
