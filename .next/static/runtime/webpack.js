e3d9b3dd5dad0614193c2dd11e30d36c65442ac7
nomaradea <nomar.adea@gmail.com>
2020-03-19 09:30:03 +0800

    setup graphql in server.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "66633882a6d2e29597b9";
+/******/ 	var hotCurrentHash = "a13a47d4f427ad518c76";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
