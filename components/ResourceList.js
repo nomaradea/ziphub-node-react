ac54fb7673f33df6a18bc76e683bcc1738f2d7d0
nomaradea <nomar.adea@gmail.com>
2020-03-19 09:57:20 +0800

    import store-js to ResourceList

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 import { Card } from '@shopify/polaris'
+import store from 'store-js'
 
 const GET_PRODUCTS_BY_ID = gql`
     query getProducts($id: [ID!]!) {
@@ -34,7 +35,9 @@ const GET_PRODUCTS_BY_ID = gql`
 class ResourceListWithProducts extends React.Component {
     render() {
         return (
-            <Query query={GET_PRODUCTS_BY_ID}>
+            <Query query={GET_PRODUCTS_BY_ID} variables={{
+                ids: store.get('ids')
+            }}>
                 {({ data, loading, error }) => {
                     if (loading) return <div>Loading...</div>
                     if (error) return <div>{error.message}</div>
